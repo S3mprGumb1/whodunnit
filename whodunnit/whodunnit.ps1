f04d80956af7a09c0c8be1e1e91a8762bea4f51f
camdo <1cysw0rdk0@gmail.com>
2018-11-26 18:45:45 -0500

    Added stub functions for filter functionality

--- whodunnit/whodunnit.ps1
+++ whodunnit/whodunnit.ps1
@@ -8,11 +8,11 @@ $menuReturn = Write-Menu -Title 'Whodunnit' -Entries @{
     }
 
     'Active Filter' = @{
-        'Export' = @(<#ExportFunc#>)
-        'Load' = @(<#LoadFunc#>)
+        'Export' = @(Export-Filter)
+        'Load' = @(Load-Filter)
         'Current Filter' = @{
-            'Username' = @(<#UsernameFilterFunc#>)
-            'Time Window' = @(<#TimeFilterFunc#>)
+            'Username' = @(Change-Filter-User)
+            'Time Window' = @(Change-Filter-Time)
             'Event Codes' = @(<#CodeFilterFunc#>)
             'Event Types' = @(<#TypeFilterFunc#>)
         }
@@ -22,3 +22,31 @@ $menuReturn = Write-Menu -Title 'Whodunnit' -Entries @{
 
     'Export Logs' = @(<#ExportLogFunc#>)
 }
+
+
+function Export-Filter {
+	<# Handles exporting the filter to a file      >
+	<  Takes a user input for the filepath         >
+	<  Then writes the current filter to the path #>	
+	Write-Output "This is a test"
+}
+
+
+function Load-Filter {
+	<# Handles loading a filter from a file                     >
+	<  Takes a user input for the filepath                      >
+	<  Then loads the filter in the file to the current filter #>	
+}
+
+
+function Change-Filter-User {
+	<# Takes a user input to change the global variable $Username #>	
+}
+
+
+function Change-Filter-Time {
+	<# Takes a user input for a start and end date (optional time format?)    >
+	<  Then updates the global variables $TimeWindowStart and $TimeWindowEnd #>	
+}
+
+
