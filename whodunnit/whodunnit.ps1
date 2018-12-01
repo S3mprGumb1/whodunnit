66ea3f79279330c50faaf62e6df8060039eac36b
camdo <1cysw0rdk0@gmail.com>
2018-12-01 17:21:19 -0500

    started boring menu

--- whodunnit/whodunnit.ps1
+++ whodunnit/whodunnit.ps1
@@ -1,34 +1,6 @@
 ﻿. $PSScriptRoot\Write-Menu\Write-Menu.ps1
 
-
-
-function Export-Filter {
-	<# Handles exporting the filter to a file      >
-	<  Takes a user input for the filepath         >
-	<  Then writes the current filter to the path #>	
-}
-
-
-function Load-Filter {
-	<# Handles loading a filter from a file                     >
-	<  Takes a user input for the filepath                      >
-	<  Then loads the filter in the file to the current filter #>	
-}
-
-
-function Change-Filter-User {
-	<# Takes a user input to change the global variable $Username #>	
-}
-
-
-function Change-Filter-Time {
-	<# Takes a user input for a start and end date (optional time format?)    >
-	<  Then updates the global variables $TimeWindowStart and $TimeWindowEnd #>	
-}
-
-
-
-
+# Menu Functions
 function Write-Lame-Menu-Main {
 
     do {
@@ -76,7 +48,7 @@ function Write-Lame-Menu-Load {
         
         switch($UserInput) {
             '1' {}
-            '2' {}
+            '2' {Read-From-Local}
             '3' {}
             '4' {Return}
         }
@@ -143,9 +115,86 @@ function Write-Lame-Menu-Filter-Edit {
 
 
 
+# Productive Functions
+function Create-Filter {
+    param ($Usernames, $TimeStart, $TimeEnd, $EventCodes, $EventTypes)
+
+    $filter = New-Object psobject
+
+    $filter | add-member -type NoteProperty -Name Usernames -Value $Usernames
+    $filter | add-member -type NoteProperty -Name TimeStart -Value $TimeStart
+    $filter | add-member -type NoteProperty -Name TimeEnd -Value $TimeEnd
+    $filter | add-member -type NoteProperty -Name EventCodes -Value $EventCodes
+    $filter | add-member -type NoteProperty -Name EventTypes -Value $EventTypes
+
+    return $filter
+}
+
+function Create-Log-Struct {
+    
+    $logs = New-Object psobject
+
+    $logs | Add-Member -type NoteProperty -Name Application -Value ""
+    $logs | Add-Member -Type NoteProperty -Name HardwareEvents -Value ""
+    $logs | Add-Member -Type NoteProperty -Name InternetExplorer -Value ""
+    $logs | Add-Member -Type NoteProperty -Name KeyManagement -Value ""
+    $logs | Add-Member -Type NoteProperty -Name OAlerts -Value ""
+    $logs | Add-Member -Type NoteProperty -Name Security -Value ""
+    $logs | Add-Member -Type NoteProperty -Name System -Value ""
+    $logs | Add-Member -Type NoteProperty -Name WindowsAzure -Value ""
+    $logs | Add-Member -Type NoteProperty -Name WindowsPowershell -Value ""
+    $logs | Add-Member -Type NoteProperty -Name Loaded -Value $false
+
+    return $logs
+}
+
+function Read-From-Local {
+    if ($Logs.Loaded) {
+        Write-Host "Logs are already loaded!"
+        $UserInput = Read-Host "Overwrite? [y/n]"
+
+        if ($UserInput -ne "y" -and $UserInput -ne "yes") {Return}
+    }
+
+    Write-Host "Loading Logs..."
+    $Logs.Application = Get-EventLog -LogName Application
+    $Logs.HardwareEvents = Get-EventLog -LogName HardwareEvents
+    $Logs.InternetExplorer = Get-EventLog -LogName 'Internet Explorer'
+    $Logs.KeyManagement = Get-EventLog -LogName 'Key Management Service'
+    $Logs.OAlerts = Get-EventLog -LogName OAlerts
+    $Logs.Security = Get-EventLog -LogName Security
+    $Logs.System = Get-EventLog -LogName System
+    $Logs.WindowsAzure = Get-EventLog -LogName 'Windows Azure'
+    $Logs.WindowsPowershell = Get-EventLog -LogName 'Windows PowerShell'
+    $Logs.Loaded = $true
+}
 
 
+# TODO
+function Export-Filter {
+	<# Handles exporting the filter to a file      >
+	<  Takes a user input for the filepath         >
+	<  Then writes the current filter to the path #>	
+}
 
+function Load-Filter {
+	<# Handles loading a filter from a file                     >
+	<  Takes a user input for the filepath                      >
+	<  Then loads the filter in the file to the current filter #>	
+}
+
+function Change-Filter-User {
+	<# Takes a user input to change the global variable $Username #>	
+}
+
+function Change-Filter-Time {
+	<# Takes a user input for a start and end date (optional time format?)    >
+	<  Then updates the global variables $TimeWindowStart and $TimeWindowEnd #>	
+}
+
+
+$CurrentFilter = Create-Filter
+$Logs = Create-Log-Struct
 Write-Lame-Menu-Main
 
 
